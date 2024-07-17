fb69835d99cc9bfa9846476d9f1d45073c265ed3
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-07-17 11:59:32 +0530

    17july

---
+++ day1.py
@@ -0,0 +1,18 @@
+# print('Hello world!')  #comment
+# num1  = 25.0
+# print(num1)
+# print(type(num1))
+
+# A - Z , a  -  z  , 0   -  9 ,   _
+# you cant start variable name declaration with numerical  value 
+# age   , Age   , AGE   three different 
+#  
+# reserved keywords ==>  
+
+# print   <== reserved 
+# flexibility  
+
+
+# print = 1000
+# print(print)
+
---
+++ day2.py
@@ -0,0 +1,181 @@
+## variable 
+# variable declares  
+# variable name rule 
+# int , float , 
+# data type 
+## __________ string _________ 
+
+
+# st = "upflairs pvt ltd 3021251h  @ #45 $"
+# st = 'upflairs pvt ltd 3021251h  @ #45 $'
+# st = """upflairs pvt ltd 3021251h  @ #45 $"""
+
+# message = """helllo 
+# how 
+# are you?
+#  where 
+#  are you
+#   from ?"""
+# print(message)
+
+# print(st)
+# print(type(st))
+
+
+
+
+# st = 'upflairs'
+
+# print(st[starting : stoping : jump])
+
+# print(st[4:7])
+# print(st[:3])  # starting  = 0 
+# print(st[2:])  # stoping  == end 
+# print(st[::1])
+# print(st[::2])   
+
+
+# st = 'upflairs'
+
+# print(st.upper())
+    # functions built in or inbuilt 
+
+
+# st = 'upflairs,pvt,ltd,jaipur,rajasthan'
+# print(st.upper())
+# print(st.lower())
+# print(st.title())
+# print(st.capitalize())
+# print(len(st))  # no. of characters count 
+# print(st.count('j'))
+# print(st.count('ja')) 
+# print(st.find('p'))  # index of the p 
+# print(st.find('jaipur'))
+# print(st.split(','))   # space 
+
+
+# st = 'upflairs,pvt,ltd,jaipur,rajasthan'
+# print(st)
+# print(st.replace(',',' '))
+
+# st = 'upflairs pvt ltd jaipur rajasthan'
+# print(st.replace('p',''))
+# print(st.endswith('s'))
+# print(st.startswith('d'))  # True
+
+# print(st.find('pvt'))
+# print(st[9:12])
+
+## data type   ___ boolean ____ 
+
+# var1 = True   
+# var2 = False 
+# print(var1,var2)
+# print(type(var1),type(var2))
+# var = False
+# print(var)
+
+
+##  list 
+# ls = [10,20,30,40,50.25,41,2.3,'upflairs',True]
+# print(ls)
+# print(type(ls))
+
+# ls = [10,20,30,40,50,60,710,80]
+# print(ls[2:])
+
+# insert , deletion , 
+# student_name = ['mohit','vikram','kanha','ram','dhakad']
+# print(type(student_name))
+# manshi
+# student_name.append('Manshi')  # to insert the item but at the last position
+# print(student_name)
+# print(student_name)
+# student_name.pop(0)  # it will remove by default from the last position
+# print(student_name)
+
+
+
+# student_name = ['mohit','vikram','kanha','ram','dhakad']
+# student_name[-1]  = "harsh dhakad"  # to update the records 
+# print(student_name)
+# student_name.append('Manshi')
+# print(student_name)
+# student_name.insert(0,'Manshi')
+# student_name.append()
+# print(student_name)
+
+
+# ls = [85,45,69,85,64,75,2,15,36,45,25,63,45]
+# print(ls.count(85))
+# print(len(ls))
+# ls.sort()
+# ls.reverse()
+# ls.index()
+# ls.clear()
+
+# ls = [10,20,30,40,50,60,True,[85,41,52,63,'upflairs'],25]
+
+# print(ls[7])
+
+# upflairs  == Flipkart 
+# ls[7][-1] = 'Flipkart'
+# print(ls)
+
+
+
+##  ------------------ TUPLE ---------------
+# mutable (changebl3)
+# imutable (non changeble)  (Tuple)
+
+# tpl = (52,41,63,52,52,85.2,41,20.01,'upflairs')
+# print(tpl)
+# print(type(tpl))
+# tpl.count()
+# tpl.index()
+# print(tpl[2:])
+# access  == yes 
+# remove == No 
+# insert == No 
+# update == no 
+
+# tpl = 10,20,30,40,50,60
+# print(tpl)
+# print(type(tpl))
+
+# tpl = with sinlge item 
+
+
+
+## ------------------ SET -------------- 
+# duplicates will not allow 
+# doesnt maintain order
+# imutable 
+# access  == NO 
+# remove == YES 
+# insert == YES 
+# update == no  
+
+
+# st = {25,41,63,96,85,855,74,45,52,63,52,52,63,6}
+# print(st[2])
+# print(type(st))
+# st.remove(520)  # raise error 
+# st.discard(520)  # not error 
+# st.add(10000)
+# print(st)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
