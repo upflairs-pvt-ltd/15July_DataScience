c17dbcf670a16b2ffde49117990c58b1323ab0b0
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-07-18 16:04:44 +0530

    '17july'

---
+++ day4.py
@@ -0,0 +1,100 @@
+### ---------- FOR LOOP ---------
+
+# for i in range(100):   # 0 - 99 
+#     print('Welcome at upflairs! ',i)
+
+
+# company = "Upflairs"
+# student_name = ['manshi','abhishek','mohit','sachin']
+# student_marks = (85,41,63,96,14,25)
+# dt = {'a':10,'b':20,'c':30,'d':40} 
+# key , value , both 
+
+# for name in student_name: 
+#     print(name)
+
+
+
+# number = [14,52,63,96,85,74,8,15,25,36,147,966,42,15,36,4]
+
+# # position == return
+# count = 0   
+# for item in number:
+#     if item == 85:
+#         print(count)
+#     count = count +1 
+
+
+#            while loop  
+
+
+# i = 0
+# while i <= 100:
+#     print("welcome at upflairs! ",i)
+#     # i = i + 1 
+#     i +=1 
+
+
+
+
+
+# for i in range(11):  
+#     if i == 5:
+#         continue 
+#     print(i)
+
+
+
+# count = 0 
+# for i in range(10):
+#     continue
+#     count += 1 
+# print(count)
+
+
+## Function 
+# age = [10,20,30,60,50,40,70,80,90]
+# age2 = [1,2,3]
+# defination  part 
+# def my_len(ls):
+#     count = 0 
+#     for i in ls:
+#         count += 1
+#     return count
+
+
+#calling part 
+# print(my_len(ls=age))  # user-defined function 
+# print(my_len(age2))
+
+# print(len(age))  # pre-defined function 
+
+
+
+# MIN , MAX 
+# ass
+# my_min() , my_max()
+
+# loop  
+# 1
+# 12
+# 123
+# 1234
+# 12345
+
+# *
+# **
+# ***
+# ****
+# *****
+# ******
+
+
+
+# Lambda function  
+
+
+
+
+
+
