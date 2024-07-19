cb14b4cde737f1989addd2ebea462e101dc19ab6
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-07-19 15:56:26 +0530

    19july

---
+++ employee.py
@@ -0,0 +1,33 @@
+# import manager    # loaded all content from manager file 
+# print(manager.assistant)
+# print(manager.assistant_id)
+# print(manager.assistant_pass)
+
+
+
+# import manager as mng     # loaded all content from manager file 
+# print(mng.assistant)
+# print(mng.assistant_pass)
+# print(mng.assistant_id)
+
+
+# from manager import assistant,assistant_id,assistant_pass 
+# print(assistant)
+# print(assistant_id)
+# print(assistant_pass) 
+# assistant = "saurabh "
+# print(assistant)
+
+
+from manager import assistant , assistant_id, assistant_pass 
+from manager import employee_data
+
+print("Employee file executed!")
+employee_data()
+# print(manager_name)
+# print(manager_id)
+# print(manager_pass) 
+# print(manager_salary)
+
+
+#
---
+++ manager.py
@@ -0,0 +1,44 @@
+## manager 
+
+
+# employee records 
+employee_list = ['Rohit','abhishek sharma','manshi','harsh']
+
+assistant = "manshi"
+assistant_id = 'manshi2545@gmail.com'
+assistant_pass = "manshi85664"
+
+employee_salary = [5200,4522,3654,7854,9696]
+def min_salary_finder(salary_ls):
+    pass 
+
+
+def employee_data():
+    print("Your employee list : ",employee_list)
+    print("Your assistant name is : ",assistant)
+    print("assistant id : ",assistant_id)
+    print("assistant password : ",assistant_pass)
+print("Manger file execute content that present outside from the condition")
+if __name__ == "__main__":
+    print("Manager file executed the content form inside the condition ")
+    manager_name = "Kishan solanki"
+    manager_id = "kishan@gmail.com"
+    manager_pass = "hello2542"
+    manager_salary = 82000
+
+    def manager_data():
+        print(manager_name)
+        print(manager_id)
+        print(manager_pass)
+        print(manager_salary)
+
+## function calling 
+# employee_data()
+# print()
+# manager_data()
+
+
+
+
+
+
