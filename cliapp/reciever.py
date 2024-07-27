de3887d86b4416d908c1cbb8d8c2c16dba0d3860
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-07-27 16:08:14 +0530

    cli project

--- cliapp/reciever.py
+++ cliapp/reciever.py
@@ -2,13 +2,22 @@ import socket
 # socket.AF_INET   --> THROUGH THE INTERNET 
 # socket.SOCK_DGRAM  --> protocol   (UDP/TCP)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-ip_address = "192.168.1.65"
+# ip_address = "192.168.1.65"
+ip_address = "127.0.0.1"
 port_no = 2525   # 0 - 65353  , 0 to 1023
 complete_address = (ip_address,port_no)
 s.bind(complete_address) 
-
+print("Hey i am listioning ...")
 while True:   # infinite loop 
-    Message = s.recvfrom(100)
+    Data = s.recvfrom(100)
+    message = Data[0]
+    message  = message.decode('ascii')
+    sender_address = Data[1]
+    sender_ip_address = sender_address[0]
+
+    
+
+
     print(Message)
 
 
--- cliapp/sender.py
+++ cliapp/sender.py
@@ -1,7 +1,7 @@
 import socket
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-target_ip = "192.168.1.65"
-# target_ip = "127.0.0.1"
+# target_ip = "192.168.1.65"
+target_ip = "127.0.0.1"  # localhost 
 target_port =  2525
 target_address = (target_ip,target_port)
 condition = True
