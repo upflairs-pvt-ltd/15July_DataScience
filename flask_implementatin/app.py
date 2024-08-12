8041b8dbf545502235b8d741f89aecb44e7d9638
Ranjit-singh-786 <jiradhey402@gmail.com>
2024-08-12 16:19:39 +0530

    flask code

--- flask_implementatin/app.py
+++ flask_implementatin/app.py
@@ -1,10 +1,37 @@
-from flask import Flask ,render_template
+from flask import Flask ,render_template,url_for,request
 app = Flask(__name__) 
 
-@app.route('/')
+@app.route('/')  # http://127.0.0.1:5000/
 def home(): 
     # return "Hii this is home page "
     return render_template('home_page.html')
+
+
+@app.route('/courses')  # http://127.0.0.1:5000/courses
+def courses(): 
+    # return "Hii this is home page "
+    return render_template('courses.html')
+
+@app.route('/contact')  # http://127.0.0.1:5000/contact
+def contact(): 
+    # return "Hii this is home page "
+    return render_template('contact.html')
+
+
+@app.route('/userdata',methods=['GET','POST'])
+def userdata():
+    if request.method == 'POST': 
+        username = request.form['username']
+        useremail = request.form['useremail']
+        usersubject = request.form['usersubject']
+        usermessage = request.form['usermessage']
+        user_recieved_data = [username,useremail,usersubject,usermessage]
+        
+        return user_recieved_data
+
+
+
+        # return "Hiii i am ready to return your data"
 
 
 if __name__ =="__main__": 
